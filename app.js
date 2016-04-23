39761696c20720a644002aa253ae5444597284a5
alu0100825985 <alu0100825985@ull.edu.es>
2016-04-23 23:27:22 +0100

    Implementando los botones para la carga de ficheros

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 "use esversion: 6";
 
-express = require('express');
-app = express();
-path = require('path');
-expressLayouts = require('express-ejs-layouts');
+const express = require('express');
+const app = express();
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/csv'); /* Iniciamos la conexión con la base de datos */
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,7 +23,24 @@ app.get('/', (request, response) => {
 });
 
 app.get('/csv', (request, response) => { //
-	response.send({"rows": calculate(request.query.input)}); // Solo se envian los datos necesarios, no una vista de ahi a que no se use render
+	response.send({"rows": calculate(request.query.input)}); // Solo se envian los datos necesarios, no una vista, de ahi a que no se use render.
+});
+ 
+const Input = require('./models/db'); // Usamos nuestro modelo.
+
+app.get('/mongo/', function(req, res) {
+    let input = new Input({
+        "name": req.query.name,
+        "content": req.query.content
+    });
+
+    input.save(function(err) {
+        if (err) {
+            console.log(`Hubieron errores:\n${err}`);
+            return err;
+        }
+        console.log(`Saved: ${input}`);
+    });
 });
 
 app.listen(app.get('port'), () => {
